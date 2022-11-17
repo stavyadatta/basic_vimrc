3a30ed4c21d1fa3990e8f53000c17fe1282f62bd
Stavya Datta <stavya.datta@awiros.com>
2022-11-17 23:23:34 +0530

    Added onedark pro and syntax highlighting to thhe vim

--- .basic_script.sh
+++ .basic_script.sh
@@ -5,3 +5,9 @@ git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-pl
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# for installing one dark theme on vim
+git clone https://github.com/joshdick/onedark.vim.git ~/onedark.vim
+mkdir -p ~/.vim/colors
+mkdir -p ~/.vim/autoload
+cp ~/onedark.vim/colors/onedark.vim ~/.vim/colors/
+cp ~/onedark.vim/autoload/onedark.vim ~/.vim/autoload/
