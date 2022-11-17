06874ef2cac93ac762a32c525c94ed04c3493786
Stavya Datta <stavya.datta@awiros.com>
2022-11-17 22:19:04 +0530

    Added Plug to basic_script.sh

--- .basic_script.sh
+++ .basic_script.sh
@@ -1,2 +1,7 @@
 mkdir -p ~/.vim/pack/git-plugins/start
 git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
+
+# This is the Plug Install command
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
