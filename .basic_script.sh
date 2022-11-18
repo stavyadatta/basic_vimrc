af13ea3289f546808dbfaea761dc4cc4dd5e9944
Stavya Datta <62042596+stavyadatta@users.noreply.github.com>
2022-11-18 11:38:27 +0530

    Added curl -y and copy for vimrc

--- .basic_script.sh
+++ .basic_script.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.vim/pack/git-plugins/start
 git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
 
 # This is the Plug Install command
-apt install curl
+apt install curl -y
 mkdir -p ~/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -12,3 +12,5 @@ git clone https://github.com/joshdick/onedark.vim.git ~/onedark.vim
 mkdir -p ~/.vim/colors
 cp ~/onedark.vim/colors/onedark.vim ~/.vim/colors/
 cp ~/onedark.vim/autoload/onedark.vim ~/.vim/autoload/
+
+cp ~/basic_vimrc/.vimrc ~/.vimrc
