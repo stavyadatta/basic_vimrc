be3e62aa5ed79cdaf217c9629833fbaf6942dac4
Stavya Datta <stavyad@student.unimelb.edu.au>
2022-11-18 05:58:07 +0000

    added curl install

--- .basic_script.sh
+++ .basic_script.sh
@@ -2,12 +2,13 @@ mkdir -p ~/.vim/pack/git-plugins/start
 git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
 
 # This is the Plug Install command
+apt install curl
+mkdir -p ~/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # for installing one dark theme on vim
 git clone https://github.com/joshdick/onedark.vim.git ~/onedark.vim
 mkdir -p ~/.vim/colors
-mkdir -p ~/.vim/autoload
 cp ~/onedark.vim/colors/onedark.vim ~/.vim/colors/
 cp ~/onedark.vim/autoload/onedark.vim ~/.vim/autoload/
